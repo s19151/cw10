ec7200285ec444dfb28bd4d7e2642c150006de66
s19151 <s19151@pjwstk.edu.pl>
2020-05-17 19:28:24 +0200

    Cw10 - Dodanie konrolera StudentsController z końcówkami do zrówcenie listy, modyfikacji studenta oraz usunięcia studenta. Zmiana ścieżki launchu

---
+++ Cwiczenia10/Cwiczenia10/Controllers/StudentsController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cwiczenia10.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cwiczenia10.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private s19151Context _dbContext;
+
+        public StudentsController(s19151Context dbContext) {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult getStudentList()
+        {
+            var studentList = _dbContext.Student.ToList();
+
+            if (studentList.Count > 0)
+                return Ok(studentList);
+
+            return NotFound();
+        }
+
+        [HttpPost("modify")]
+        public IActionResult modifyStudent(Student student)
+        {
+            var s = _dbContext.Student.Find(student.IndexNumber);
+
+            if (s != null)
+            {
+                _dbContext.Entry(s).CurrentValues.SetValues(student);
+                _dbContext.SaveChanges();
+
+                return Ok(s);
+            }
+
+            return BadRequest("Podany student nie istnieje");
+        }
+
+        [HttpDelete("delete/{id}")]
+        public IActionResult deleteStudent(string id)
+        {
+            var student = _dbContext.Student.Find(id);
+
+            if (student != null)
+            {
+                _dbContext.Student.Remove(student);
+                _dbContext.SaveChanges();
+
+                return Ok(student);
+            }
+
+            return BadRequest("Brak podanego studenta");
+        }
+    }
+}
--- Cwiczenia10/Cwiczenia10/Startup.cs
+++ Cwiczenia10/Cwiczenia10/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cwiczenia10.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -25,6 +26,8 @@ namespace Cwiczenia10
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSingleton<s19151Context, s19151Context>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
