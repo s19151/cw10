ebf534309b5691e3a619d84e29691d3eea940d5c
s19151 <s19151@pjwstk.edu.pl>
2020-05-17 21:05:49 +0200

    Cw10 - Dodanie kontrolera EnrollmentsController z końcówkami do obsługi rządań zapisu studenta oraz promocji studentów

---
+++ Cwiczenia10/Cwiczenia10/Controllers/EnrollmentsController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cwiczenia10.DTO.Requests;
+using Cwiczenia10.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cwiczenia10.Controllers
+{
+    [Route("api/enrollments")]
+    [ApiController]
+    public class EnrollmentsController : ControllerBase
+    {
+        private s19151Context _dbContext;
+
+        public EnrollmentsController(s19151Context dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public IActionResult EnrollStudent(EnrollStudentRequest request)
+        {
+            var student = _dbContext.Student.Find(request.IndexNumber);
+
+            if (student != null)
+                return BadRequest("Podano zły nr indeksu");
+
+            var studies = _dbContext.Studies.Where(s => s.Name.Equals(request.Studies));
+
+            if (studies.Count() == 0)
+                return NotFound("Podane studia nie istnieją");
+
+            var enrollments = _dbContext.Enrollment.Where(en => en.IdStudy == studies.First().IdStudy && en.Semester == 1)
+                .OrderByDescending(en => en.StartDate);
+            Enrollment enrollment;
+            if (enrollments.Count() == 0)
+            {
+                enrollment = new Enrollment();
+
+                enrollment.IdEnrollment = Convert.ToInt32(_dbContext.Enrollment.Max(en => en.IdEnrollment)) + 1;
+                enrollment.Semester = 1;
+                enrollment.IdStudy = studies.First().IdStudy;
+                enrollment.StartDate = DateTime.Now.Date;
+
+                _dbContext.Enrollment.Add(enrollment);
+            }
+            else
+            {
+                enrollment = enrollments.First();
+            }
+
+            //dodanie studenta
+            student = new Student();
+
+            student.IndexNumber = request.IndexNumber;
+            student.FirstName = request.FirstName;
+            student.LastName = request.LastName;
+            student.BirthDate = DateTime.Parse(request.BirthDate);
+            student.IdEnrollment = enrollment.IdEnrollment;
+
+            _dbContext.Student.Add(student);
+
+            _dbContext.SaveChanges();
+
+            //zwrócenie enrollment wyrzuca błąd jsona
+            //return Created("", enrollment);
+
+            return Ok("działa");
+        }
+
+        [HttpPost("promotions")]
+        public IActionResult PromoteStudent(PromoteStudentsRequest request)
+        {
+            var studies = _dbContext.Studies.Where(st => st.Name.Equals(request.Studies));
+
+            if (studies.Count() == 0)
+                return NotFound("Brak studiów o podanym kierunku");
+
+            var study = studies.First();
+
+            var enrollments = _dbContext.Enrollment.Where(en => en.IdStudy == study.IdStudy && en.Semester == request.Semester);
+
+            if (enrollments.Count() == 0)
+                return NotFound("Brak wpisów na 1 semestr dla podanego kierunku");
+
+            var id = enrollments.First().IdEnrollment;
+
+            _dbContext.Database.ExecuteSqlRaw($"promotestudents {request.Studies} {request.Semester}");
+
+            _dbContext.SaveChanges();
+
+            var enrollment = _dbContext.Enrollment.Find(id);
+
+            //to samo
+            //return Created("", enrollment);
+
+            return Ok(id);
+        }
+    }
+}
---
+++ Cwiczenia10/Cwiczenia10/DTO/Requests/PromoteStudentRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cwiczenia10.DTO.Requests
+{
+    public class PromoteStudentsRequest
+    {
+        public String Studies { get; set; }
+        public int Semester { get; set; }
+    }
+}
